1be5ca8b9bacded1bf5f60799b130fe9ba7f9c2d
zhenjess <jessicazhen8@gmail.com>
2020-01-30 18:43:49 -0800

    Iterate through cart items array

--- frontend/reducers/cart_items_reducer.js
+++ frontend/reducers/cart_items_reducer.js
@@ -9,22 +9,24 @@ const cartItemsReducer = (oldState = {}, action) => { //oldState=[]
     //debugger
     //let newState = oldState.slice(0);
     let newState;
-    switch(action.type) {
+    switch (action.type) {
         case RECEIVE_CART_ITEMS:
-             newState = {};
-             debugger
-            Object.values(action.cartItems.cartItems).forEach(cartItem => {
+            newState = {};
+            // debugger
+            let cartItemsArr = action.cartItems.cartItems ? Object.values(action.cartItems.cartItems) : [];
+            if (cartItemsArr.length === 0) return oldState;
+            cartItemsArr.forEach(cartItem => {
                 newState[cartItem.id] = cartItem
-            }); 
+            });
             // return Object.assign({}, oldState, newState); 
             return newState; //this returns all cartItems
 
         case RECEIVE_CART_ITEM:
-          // debugger
-           // newState.push(action.cartItem); 
+            // debugger
+            // newState.push(action.cartItem); 
             // newState = Object.assign({}, oldState, action.cartItem);
             newState = Object.assign({}, oldState, { [action.cartItem.id]: action.cartItem });
-           // newState.push(action.cartItem); 
+            // newState.push(action.cartItem); 
             return newState;
         case RECEIVE_ALL_SHOES:
             newState = Object.assign({}, oldState, action.payload.cartItems);
@@ -33,7 +35,7 @@ const cartItemsReducer = (oldState = {}, action) => { //oldState=[]
             newState = Object.assign({}, oldState, action.cartItems);
             return newState;
         case REMOVE_CART_ITEM:
-             //debugger
+            debugger
             newState = Object.assign({}, oldState);
             // delete newState[action.cartItem.id];
             delete newState[action.cartItemId];
