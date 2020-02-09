2327a0f14dd51255f8acec1bfbdd568d38afab74
zhenjess <jessicazhen8@gmail.com>
2020-02-09 00:22:16 -0800

    Add clear errors cases to errors type reducers

--- frontend/reducers/cart_items_errors_reducer.js
+++ frontend/reducers/cart_items_errors_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_CART_ITEM, RECEIVE_CART_ITEM_ERRORS } from '../actions/cart_items_actions';
+import { CLEAR_ERRORS } from '../actions/session_actions';
 
 const cartItemsErrorsReducer = (oldState=[], action) => {
     Object.freeze(oldState);
@@ -11,6 +12,8 @@ const cartItemsErrorsReducer = (oldState=[], action) => {
         case RECEIVE_CART_ITEM_ERRORS:
             newState = newState.concat(action.errors);
             return newState;
+        case CLEAR_ERRORS:
+            return [];
         default:
             return oldState;
     }
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,6 +1,7 @@
 import { 
     RECEIVE_SESSION_ERRORS, 
-    RECEIVE_CURRENT_USER 
+    RECEIVE_CURRENT_USER,
+    CLEAR_ERRORS 
 } from '../actions/session_actions';
 
 const sessionErrorsReducer = (state=[], action) => {
@@ -11,6 +12,8 @@ const sessionErrorsReducer = (state=[], action) => {
             return action.errors;
         case RECEIVE_CURRENT_USER:
             return [];
+        case CLEAR_ERRORS:
+            return[];
         default:
             return state;
     }
