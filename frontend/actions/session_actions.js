66974af75dc5d903a9ebca524f607fef7e5f7547
zhenjess <jessicazhen8@gmail.com>
2020-02-09 00:18:21 -0800

    Create clear errors action

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -5,6 +5,7 @@ import * as CartApiUtil from '../util/cart_api_util';
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
 const receiveCurrentUser = (currentUser) => ({
     type: RECEIVE_CURRENT_USER, 
@@ -44,6 +45,12 @@ export const logout = () => dispatch => {
             .then(() => dispatch(logoutCurrentUser()))
     );
 };
+
+export const clearErrors = () => (
+    ({
+        type: CLEAR_ERRORS
+    })
+);
 
 // export const fetchCart = (user, cartId) => dispatch => {
 //     return (
