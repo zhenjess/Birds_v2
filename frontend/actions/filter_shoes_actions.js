fb95cfe9f943a66844d2ae26bd4a559ea72c9faf
zhenjess <jessicazhen8@gmail.com>
2020-03-30 11:43:03 -0700

    Create the normal filter actions

--- frontend/actions/filter_shoes_actions.js
+++ frontend/actions/filter_shoes_actions.js
@@ -1,3 +1,18 @@
 export const FILTER_BY_COLOR = 'FILTER_BY_COLOR';
 export const FILTER_BY_STYLE = 'FILTER_BY_STYLE';
 export const FILTER_BY_MODEL = 'FILTER_BY_MODEL';
+
+const filterByColor = payload => ({
+    type: FILTER_BY_COLOR,
+    payload
+});
+
+const filterByStyle = payload => ({
+    type: FILTER_BY_STYLE,
+    payload
+});
+
+const filterByModel = payload => ({
+    type: FILTER_BY_MODEL,
+    payload
+});
