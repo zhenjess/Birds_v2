143f57feb4ea18fee2322bbe4c66423bd52ef825
zhenjess <jessicazhen8@gmail.com>
2020-05-16 16:44:29 -0700

    initialize animations

--- frontend/components/shoes/shoe_index.jsx
+++ frontend/components/shoes/shoe_index.jsx
@@ -4,3 +4,17 @@ import ShoeIndexItem from './shoe_index_item';
 import CartItem from '../modal/cart_item';
 import ShoesHeader from './shoes_header';
 
+class ShoeIndex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            shouldAnimate: false,
+            animateItems: true,
+            animateNotification: false,
+            cart: {},
+        };
+        this.handleAnimationEnd = this.handleAnimationEnd.bind(this);
+        this.startNotification = this.startNotification.bind(this);
+        this.endNotification = this.endNotification.bind(this);
+    }
+}
