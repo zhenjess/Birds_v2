41ef295d928313bd9dfafb8e1f0e8f84bbdc53e6
zhenjess <jessicazhen8@gmail.com>
2020-05-16 17:27:02 -0700

    Remove route from shoe index

--- frontend/components/shoes/shoe_index.jsx
+++ frontend/components/shoes/shoe_index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import ShoeIndexItem from './shoe_index_item';
 import CartItem from '../modal/cart_item';
 import ShoesHeader from './shoes_header';
